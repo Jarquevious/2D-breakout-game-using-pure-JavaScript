a090d125b3f77eecf38d4aa835e77cd5e7715662
Jarquevious <jarquevious@gmail.com>
2020-11-06 16:55:39 -0800

    game complete

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 // JavaScript code goes here
 
 // ====================================================
-//                        variants
+//                        let  iants
 // ====================================================
 
-const canvas = document.getElementById("myCanvas");
-let ctx = canvas.getContext("2d");
-let ballRadius = 10;
-let x = canvas.width/2;
-let y = canvas.height-30;
-let dx = 2;
-let dy = -2;
-let paddleHeight = 10;
-let paddleWidth = 75;
-let paddleX = (canvas.width-paddleWidth)/2;
-let rightPressed = false;
-let leftPressed = false;
-let brickRowCount = 5;
-let brickColumnCount = 3;
-let brickWidth = 75;
-let brickHeight = 20;
-let brickPadding = 10;
-let brickOffsetTop = 30;
-let brickOffsetLeft = 30;
-let score = 0;
-let lives = 3;
+let   canvas = document.getElementById("myCanvas");
+let   ctx = canvas.getContext("2d");
+let   ballRadius = 10;
+let   x = canvas.width/2;
+let   y = canvas.height-30;
+let   dx = 2;
+let   dy = -2;
+let   paddleHeight = 10;
+let   paddleWidth = 75;
+let   paddleX = (canvas.width-paddleWidth)/2;
+let   rightPressed = false;
+let   leftPressed = false;
+let   brickRowCount = 5;
+let   brickColumnCount = 3;
+let   brickWidth = 75;
+let   brickHeight = 20;
+let   brickPadding = 10;
+let   brickOffsetTop = 30;
+let   brickOffsetLeft = 30;
+let   score = 0;
+let   lives = 3;
 
 let bricks = [];
 for (let c=0; c<brickColumnCount; c++) {
@@ -75,7 +75,6 @@ function collisionDetection() {
           if (score == brickRowCount*brickColumnCount) {
             alert("YOU WIN, CONGRATS!");
             document.location.reload();
-            clearInterval(interval); // Needed for  Chrome to end game
           }
         }
       }
@@ -132,6 +131,7 @@ function draw() {
   drawBall();
   drawPaddle();
   drawScore();
+  drawLives();
   collisionDetection();
 
   if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
@@ -139,16 +139,26 @@ function draw() {
   }
   if (y + dy < ballRadius) {
     dy = -dy;
-  } else if (y + dy > canvas.height-ballRadius) {
+  } 
+  else if (y + dy > canvas.height-ballRadius) {
     if (x > paddleX && x < paddleX + paddleWidth) {
       dy = -dy;
-    } else {
-      alert('GAME OVER');
-      document.location.reload();
-      clearInterval(interval); // Needed for  Chrome to end game
+    } 
+    else {
+      lives--;
+      if(!lives) {
+        alert("GAME OVER");
+        document.location.reload();
+  }
+      else {
+        x = canvas.width/2;
+        y = canvas.height-30;
+        dx = 3;
+        dy = -3;
+        paddleX = (canvas.width-paddleWidth)/2;
+      }
     }
   }
-
   if (rightPressed && paddleX < canvas.width - paddleWidth) {
     paddleX += 7;
   }
@@ -158,6 +168,9 @@ function draw() {
 
   x += dx;
   y += dy;
+  requestAnimationFrame(draw);
+
 }
 
-let interval = setInterval(draw, 10);
+draw()
+
